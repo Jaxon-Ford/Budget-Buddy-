c39a78031f337f0c9ea6316475e32fc3a6afd043
Luis <acunafernando617@gmail.com>
2026-03-18 10:07:59 -0500

    checking how much of my progress was undone

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-from flask import Flask, jsonify, request, session, render_template
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-app.secret_key = "super duper secret key"
 
-@app.route("/", methods=["POST"])
+app.route("/")
 def home():
     None
-
-@app.route("/summary")
-def summary():
-    None
-
-
-def clear():
-    session.clear()
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True, use_reloader=False)
